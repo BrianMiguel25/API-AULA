57d662f82736fb1db8989c1743ac9fb45c38dbd5
BRIAN MIGUEL DO NASCIMENTO SANTOS <brian.santos@cs.cruzeirodosul.edu.br>
2026-09-17 10:17:23 -0300

    Criação de arquiteturas em camadas

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const chamadoRoutes = require('../API-Aula/routes/chamadoRoutes')
+const app = express();
+const PORT = 3000
+
+
+app.use(express.json());
+
+app.use('/chamados', chamadoRoutes)
+
+app.listen(PORT, function(){
+    console.log('Servidor rodando na porta ' + PORT);
+})
